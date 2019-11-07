40993cb3c18a25baf76b404ecd1b430e7b53e082
Ricardo Beltran <richardb9207@gmail.com>
2019-11-07 04:49:41 -0500

    querys, cors, deploys ok

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const { config }= require('./config/index');
 const customerLeakApi = require('./routes/customers.js');
 
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
 customerLeakApi(app);
 
 app.listen(config.port, function(){
